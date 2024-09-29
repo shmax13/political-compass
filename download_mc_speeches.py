d996a7b6efa6b3d7dff8b06548016836cb9ae5bd
Maximilian Pfeil <67435950+shmax13@users.noreply.github.com>
2024-09-29 09:57:41 +0000

    add MC corpus + download script

---
+++ download_mc_speeches.py
@@ -0,0 +1,22 @@
+# Miller Center of Public Affairs, University of Virginia. "Presidential Speeches: Downloadable Data." Accessed March 17, 2022. data.millercenter.org.
+# https://data.millercenter.org
+
+import json, requests, sys
+
+endpoint = "https://api.millercenter.org/speeches"
+out_file = "speeches.json"
+
+r = requests.post(url=endpoint)
+data = r.json()
+items = data['Items']
+
+while 'LastEvaluatedKey' in data:
+    parameters = {"LastEvaluatedKey": data['LastEvaluatedKey']['doc_name']}
+    r = requests.post(url = endpoint, params = parameters)
+    data = r.json()
+    items += data['Items']
+    print(f'{len(items)} speeches')
+
+with open(out_file, "w") as out:
+    out.write(json.dumps(items))
+    print(f'wrote results to file: {out_file}')
