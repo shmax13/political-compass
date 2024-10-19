03047a6d22bd0f84daca76936c4c857f0249d91c
almaESL <almli825@student.liu.se>
2024-10-19 20:00:38 +0000

    NER in preproc. + playing with PCA in tfidf and word2vec

--- feature_extraction/word2vec.py
+++ feature_extraction/word2vec.py
@@ -3,6 +3,7 @@ import numpy as np
 import gensim.downloader as api
 from sklearn.model_selection import train_test_split
 from .base import BaseFeatureExtractor
+from sklearn.decomposition import PCA
 
 class Word2VecExtractor(BaseFeatureExtractor):
     def __init__(self):
@@ -16,10 +17,19 @@ class Word2VecExtractor(BaseFeatureExtractor):
         word_vectors = [self.model[word] for word in words if word in self.model]
         if len(word_vectors) == 0:
             return np.zeros(300)  # 300 is the dimension of Word2Vec
-        return np.mean(word_vectors, axis=0)
+        # return np.mean(word_vectors, axis=0)
+        weights = np.array([1 / len(word_vectors)] * len(word_vectors))
+        return np.average(word_vectors, axis=0, weights=weights)
 
     def extract(self, cleaned_speeches, labels):
         """Extract Word2Vec features."""
         X_word2vec = np.array([self.vectorize_speech(speech) for speech in cleaned_speeches])
+
+        pca = PCA(n_components=200)
+        X_word2vec = pca.fit_transform(X_word2vec)
+
         X_train_word2vec, X_test_word2vec, y_train, y_test = train_test_split(X_word2vec, labels, test_size=0.2, random_state=42)
         return X_train_word2vec, X_test_word2vec, y_train, y_test
+   
+
+    
